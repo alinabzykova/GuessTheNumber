df6f74d69cb8b07d45411c9afa0a519ea99e06d6
LENOVO-PC <LENOVO-PC@DESKTOP-7D27R3T>
2025-06-03 11:42:44 +0300

    Добавьте файлы проекта.

---
+++ GuessTheNumber/MainForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessTheNumber
+{
+    public partial class MainForm : Form
+    {
+        GameEngine engine = new GameEngine();
+        PlayerStats stats = PlayerStats.Load();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            engine.StartNewGame();
+            resultLabel.Text = "";
+        }
+
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(inputBox.Text, out int guess))
+            {
+                string result = engine.CheckGuess(guess);
+                resultLabel.Text = result;
+
+                if (result == "Угадал")
+                {
+                    stats.Update(engine.Attempts);
+                    stats.Save();
+
+                    ResultForm rf = new ResultForm(engine.Attempts, stats);
+                    rf.ShowDialog();
+                    engine.StartNewGame();
+                    inputBox.Clear();
+                    resultLabel.Text = "";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите целое число!");
+            }
+        }
+    }
+}
---
+++ GuessTheNumber/PlayerStats.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace GuessTheNumber
+{
+    public class PlayerStats
+    {
+        public int GamesPlayed { get; set; }
+        public int GamesWon { get; set; }
+        public int TotalAttempts { get; set; }
+
+        private const string FilePath = "stats.json";
+
+        public static PlayerStats Load()
+        {
+            if (File.Exists(FilePath))
+            {
+                string json = File.ReadAllText(FilePath);
+                return JsonConvert.DeserializeObject<PlayerStats>(json);
+            }
+            return new PlayerStats();
+        }
+
+        public void Save()
+        {
+            string json = JsonConvert.SerializeObject(this);
+            File.WriteAllText(FilePath, json);
+        }
+
+        public void Update(int attempts)
+        {
+            GamesPlayed++;
+            GamesWon++;
+            TotalAttempts += attempts;
+        }
+    }
+}
