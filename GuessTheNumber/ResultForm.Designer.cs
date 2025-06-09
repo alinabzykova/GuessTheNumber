019db370ff1c964865d1ced0d9219f7632cc0934
admin <admin@DESKTOP-M5T76M4>
2025-06-09 17:35:06 +0300

    Добавьте файлы проекта.

---
+++ GuessTheNumber/ResultForm.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace GuessTheNumber
+{
+    partial class ResultForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.topPlayersGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.topPlayersGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(179, 156);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(179, 251);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(144, 350);
+            this.closeButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(100, 28);
+            this.closeButton.TabIndex = 2;
+            this.closeButton.Text = "Закрыть";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // topPlayersGrid
+            // 
+            this.topPlayersGrid.AllowUserToAddRows = false;
+            this.topPlayersGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.topPlayersGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.topPlayersGrid.Location = new System.Drawing.Point(556, 208);
+            this.topPlayersGrid.Name = "topPlayersGrid";
+            this.topPlayersGrid.ReadOnly = true;
+            this.topPlayersGrid.RowHeadersWidth = 51;
+            this.topPlayersGrid.RowTemplate.Height = 24;
+            this.topPlayersGrid.Size = new System.Drawing.Size(407, 283);
+            this.topPlayersGrid.TabIndex = 3;
+            // 
+            // ResultForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.topPlayersGrid);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "ResultForm";
+            this.Text = "ResultForm";
+            this.Load += new System.EventHandler(this.ResultForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.topPlayersGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.DataGridView topPlayersGrid;
+    }
+}
